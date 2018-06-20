6f4aa3f8ba031163b83b94feeac8e32f2285dbee
timzprof <timilehin.olumofin@gmail.com>
2018-06-20 21:13:29 +0100

    This is my First React App

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id:1,
+	name:'Elijah Michaelson',
+	username: 'Savage',
+	email:'reddoor@gmail.com'
+},
+{
+	id:2,
+	name: 'Klaus Michaelson',
+	username: 'Hybrid Bastard',
+	email:'daddyissues@yahoo.com'
+},
+{
+	id:3,
+	name: 'Rebeekah Michaelson',
+	username: 'Love Nurse',
+	email: 'marsellous@hotmail.com'
+},
+{
+	id:4,
+	name: 'Hope Michaelson',
+	username: 'Miracle Baby',
+	email: 'mumuinternational@gmail.com'
+},
+{
+	id:5,
+	name: 'Papa Sly',
+	username: 'Mr Andela',
+	email: 'android@andela.com'
+},
+{
+	id:6,
+	name: 'Slvester The First',
+	username: 'Baby Skin Predator',
+	email: 'slyo@gmail.com'
+},
+{
+	id:7,
+	name: 'ProfT',
+	username: 'JS Enthusiast',
+	email:'prof@gmail.com'
+},
+{
+	id:8,
+	name: 'IBK',
+	username: 'Dead Guy',
+	email: 'earpiece@mumumail.com'
+},
+{
+	id:9,
+	name: 'K money',
+	username: 'Oga n la',
+	email: 'cashflowing@oga.com'
+},
+{
+	id:10,
+	name: 'Mary Silver',
+	username: 'Maryo',
+	email:'mary@silver.com'
+}
+];
